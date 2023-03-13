38432cc576546c0b5dd37aa97f999d0026989a37
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-12 22:16:35 -0400

    printUserWithBalance function

--- bank-account-interfaces/main.go
+++ bank-account-interfaces/main.go
@@ -9,12 +9,20 @@ import (
 
 func main() {
 	//router.InitializeRouter()
-	cuentaBolivares := models.NewBolivaresAccount()
-	cuentaDolares := models.NewUSDaccount()
-	printBalance(cuentaDolares)
-	printBalance(cuentaBolivares)
+	bolivares := models.NewBolivaresAccount()
+	dolares := models.NewUSDaccount()
+	user1 := models.NewUser().SetName("Oscar Pernia").AddBankAccounts(bolivares, dolares)
+	printUserWithBalances(user1)
+	bolivares.Deposit(100)
+	printUserWithBalances(user1)
 }
 
 func printBalance(account interfaces.IBankAccounts) {
 	fmt.Printf("Usted tiene %d en su cuenta de %s\n", account.GetBalance(), account.GetCurrency())
+}
+
+func printUserWithBalances(user *models.User) {
+	for _, account := range user.GetBankAccounts() {
+		fmt.Printf("este usuario %s tiene %d en su cuenta de %s\n", user.GetName(), account.GetBalance(), account.GetCurrency())
+	}
 }
