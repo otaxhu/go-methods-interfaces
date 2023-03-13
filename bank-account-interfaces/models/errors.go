0e57ac1089abee128c06a2cdd68266a8314b55b5
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-12 22:24:20 -0400

    errCurrencyAccountNotFound

--- bank-account-interfaces/models/errors.go
+++ bank-account-interfaces/models/errors.go
@@ -3,6 +3,7 @@ package models
 import "errors"
 
 var (
-	errInsufficientFunds = errors.New("fondos insuficientes")
-	errInvalidAmount     = errors.New("monto invalido")
+	errInsufficientFunds       = errors.New("fondos insuficientes")
+	errInvalidAmount           = errors.New("monto invalido")
+	errCurrencyAccountNotFound = errors.New("no se encontró una cuenta con la moneda especificada")
 )
