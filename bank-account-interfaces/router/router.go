480b2ac2a823c8736b53cb1dc834fe346da99877
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-13 19:56:30 -0400

    automigration, loading .env and /signup endpoint

--- bank-account-interfaces/router/router.go
+++ bank-account-interfaces/router/router.go
@@ -3,20 +3,36 @@ package router
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/otaxhu/go-methods-interfaces/bank-account-interfaces/controllers"
+	"github.com/otaxhu/go-methods-interfaces/bank-account-interfaces/database"
+	"github.com/otaxhu/go-methods-interfaces/bank-account-interfaces/models"
 )
 
 func newServer(handler http.Handler) *http.Server {
-	return &http.Server{Addr: ":8080", Handler: handler}
+	return &http.Server{Addr: os.Getenv("server_port"), Handler: handler}
 }
 
 func InitializeRouter() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal(err)
+	}
+	tx, err := database.NewTransaction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := tx.AutoMigrate(&models.User{}, &models.BankAccount{}); err != nil {
+		log.Fatal(err)
+	}
+	tx.Commit()
 	router := mux.NewRouter()
 	server := newServer(router)
 
 	router.HandleFunc("/", controllers.Home).Methods(http.MethodGet)
+	router.HandleFunc("/signup", controllers.RegisterUser).Methods(http.MethodPost)
 
 	log.Fatal(server.ListenAndServe())
 }
