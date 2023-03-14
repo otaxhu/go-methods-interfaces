6d5d42bc42ac128f0256d49890d14a4ce206b4b6
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-13 20:08:48 -0400

    changed the name of the file errors.go to const.go and all the constructor functions of BankAccounts moved to bank-account.go

--- bank-account-interfaces/models/errors.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "errors"
-
-var (
-	errInsufficientFunds       = errors.New("fondos insuficientes")
-	errInvalidAmount           = errors.New("monto invalido")
-	errCurrencyAccountNotFound = errors.New("no se encontró una cuenta con la moneda especificada")
-)
